583f20798cadaa5f1a362df54ea3884b77e1de92
Vladimir <vovaykunin@gmail.com>
2023-02-16 07:27:16 +0300

    Сделанно 3 задачи

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,53 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Домашнее задание 4");
+
+        System.out.println("Задание 1");
+        int rub = 1000000;
+        System.out.println(rub);
+        byte apple = 100;
+        System.out.println(apple);
+        short lison = 10000;
+        System.out.println(lison);
+        long life = 1000000000L;
+        System.out.println(life);
+        float pi = 3.14f;
+        System.out.println(pi);
+        double year = 3.15;
+        System.out.println(year);
+
+        System.out.println("Задание 2");
+
+        float guru = 27.12f;
+        System.out.println(guru);
+        long look = 987678965549L;
+        System.out.println(look);
+        double like = 2.786;
+        System.out.println(like);
+        short li = 569;
+        System.out.println(li);
+        short di = -159;
+        System.out.println(di);
+        int hiPi = 27897;
+        System.out.println(hiPi);
+        byte ni = 67;
+        System.out.println(ni);
+
+        System.out.println("Задание 3");
+
+        byte ludmila = 23;
+        byte anna = 27;
+        byte ekaterina = 30;
+        short papers = 480;
+        int totalStudents = ludmila + anna + ekaterina;
+        int totalList = papers / totalStudents;
+        System.out.println("На каждого ученика рассчитано " + totalList + " листов бумаги.");
+
+        System.out.println("Задание 4");
+
+
+
+
     }
 }
