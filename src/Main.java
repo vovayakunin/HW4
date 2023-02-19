b3f7a1b715201cfd7ae149b8bb6c9a25255b29f3
Vladimir <vovaykunin@gmail.com>
2023-02-19 22:39:52 +0300

    Все сделанно

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.logging.SocketHandler;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -45,6 +47,92 @@ public class Main {
         System.out.println("На каждого ученика рассчитано " + totalList + " листов бумаги.");
 
         System.out.println("Задание 4");
+
+        byte pro = 16;
+        byte time = 2;
+        int oneTime = pro / time;
+        byte min = 20;
+        int twenty = min * oneTime;
+        byte hour = 60;
+        byte hours = 24;
+        int day = (oneTime * hour) * hours;
+        byte three = 3;
+        int threeDay = day * three;
+        byte month = 30;
+        int oneMonth = day * month;
+
+        System.out.println("За 20 минут машина произвела " + twenty + " штук бутылок.");
+        System.out.println("За сутки машина произвела " + day + " штук бутылок.");
+        System.out.println("За 3 дня машина произвела " + threeDay + " штук бутылок.");
+        System.out.println("За 1 месяц машина произвела " + oneMonth + " штук бутылок.");
+
+        System.out.println("Задание 5");
+
+        byte white = 2;
+        byte brown = 4;
+        byte pot = 120;
+        int oneClass = white + brown;
+        int klass = pot / oneClass;
+        int whitePot = white * klass;
+        int brownPot = brown * klass;
+        System.out.println("В школе, где " + klass + " классов, нужно " + whitePot + " банок белой краски и " + brownPot + " банок коричневой краски.");
+
+        System.out.println("Задание 6");
+
+        byte fiveBanana = 5;
+        byte banana = 80;
+        int totalBanana = fiveBanana * banana;
+        short milk = 105 * 2;
+        short ice = 100 * 2;
+        short eggs = 4 * 70;
+        int wtGr = eggs + ice + milk + totalBanana;
+        int grPerKg = 1000;
+        float weightKg = wtGr / (float)grPerKg;
+        System.out.println("Вес спортзавтрака равен " + wtGr + " грамм");
+        System.out.println("Вес спортзавтрака равен " + weightKg + " килограмм");
+
+        System.out.println("Задание 7");
+
+        short numberOne = 250;
+        short numberTwo = 500;
+        byte weight = 7;
+        short weightGr = 1000;
+        int totalGr = weightGr * weight;
+        int optionOne = totalGr / numberOne;
+        int optionTwo = totalGr / numberTwo;
+        int optionOneTwo = optionTwo + optionOne;
+        int average = optionOneTwo / 2;
+        System.out.println("Спортсмену нужно " + optionOne + " дней, если он будет терять каждый день по " + numberOne + " грамм.");
+        System.out.println("Спортсмену нужно " + optionTwo + " дней, если он будет терять каждый день по " + numberTwo + " грамм.");
+        System.out.println("Спортсмену нужно " + average + " дней в среднем, чтобы добиться результата похудения.");
+
+        System.out.println("Задание 8");
+
+        int masha = 67760;
+        int denis = 83690;
+        int kristina = 76230;
+        byte oneYear = 12;
+        double percent = 0.1;
+        double mashaPercent = masha * percent;
+        double denisPercent = denis * percent;
+        double kristinaPercent = kristina * percent;
+        double newMasha = masha + mashaPercent;
+        double newDenis = denis + denisPercent;
+        double newKristina = kristina + kristinaPercent;
+        double moy = mashaPercent * oneYear;
+        double doy = denisPercent * oneYear;
+        double koy = kristinaPercent * oneYear;
+        System.out.println("Маша теперь получает " + newMasha + " рублей. Годовой доход вырос на " + moy + " рублей.");
+        System.out.println("Денис теперь получает " + newDenis + " рублей. Годовой доход вырос на " + doy + " рублей.");
+        System.out.println("Кристина теперь получает " + newKristina + " рублей. Годовой доход вырос на " + koy + " рублей.");
+
+
+
+
+
+
+
+
 
 
 
